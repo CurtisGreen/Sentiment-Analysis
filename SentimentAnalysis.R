7748a907b1f44dd4029dddfa9041a06bd98dc130
Curtis Green <greencur000@email.tamu.edu>
2018-04-29 14:50:12 -0500

    Improved accuracy, made more readable

--- SentimentAnalysis.R
+++ SentimentAnalysis.R
@@ -19,7 +19,6 @@ data = data[,-2]
 colnames(data)[2] = "emotion"
 data = data[data$emotion == "Negative emotion" | data$emotion == "Positive emotion",]
 data = data %>% mutate(index = row_number())
-data$total = str_count(data$tweet_text, '\\s+')+1
 
 # Seperate by word
 parsed_tweets = tidytext::unnest_tokens(data, word, tweet_text)
@@ -145,12 +144,16 @@ train = sample(data_size,train_size)
 svmradi = svm(y~.,data=dat[train,], kernel="radial", gamma=1, cost=1)
 plot(svmradi, dat[train,])
 
+# Predict with radial
+pred = predict(svmradi,dat[-train,])
+table(predict = pred, truth = dat[-train,]$y)
+
 # Tune radial model
 tune.out = tune(svm,y~., data=dat[train,], kernel="radial", ranges = list(cost=10^(-1:1),gamma=c(0.5,1:4)))
 best = tune.out$best.model
 plot(best,dat[train,])
 
-# Predict with radial
+# Predict with best radial
 pred = predict(best,dat[-train,])
 table(predict = pred, truth = dat[-train,]$y)
 
@@ -168,10 +171,15 @@ colors = c("darkred", "darkgreen")
 barplot(counts, main = "Emotion distribution", ylab = "Count",
         col = colors)
 
-# Get all words in both dictionaries
+# Get all words in 3 dictionaries
+nrc = get_sentiments("nrc") %>%
+  filter(sentiment %in% c("positive",
+                              "negative"))
+bing_nrc_sent = rbind(get_sentiments("bing"), nrc)
+bing_nrc_sent = bing_nrc_sent[!duplicated(bing_nrc_sent$word),]
+all_words = inner_join(get_sentiments("afinn"), bing_nrc_sent)
 all_words = parsed_tweets %>%
-  inner_join(get_sentiments("afinn"))%>%
-  inner_join(get_sentiments("bing"))
+  inner_join(all_words)
 
 # Change text into binary
 all_words = all_words[!duplicated(all_words$word),]
@@ -180,22 +188,30 @@ all_words$emotion[all_words$emotion == "Positive emotion"] = 1
 all_words$sentiment[all_words$sentiment == "negative"] = -1
 all_words$sentiment[all_words$sentiment == "positive"] = 1
 
-test = inner_join(parsed_tweets, all_words, by = "word")
-colnames(test)[1] = "afinn_tfidf"
-test$afinn_tfidf = 0
-test_table = as.data.frame(table(test$word))
-colnames(test_table)[1] = "word"
-testerino = right_join(test, test_table, by = "word")
-
+# Append frequency to data
+freq_append = inner_join(parsed_tweets, all_words, by = "word")
+colnames(freq_append)[1] = "afinn_tfidf"
+freq_append$afinn_tfidf = 0
+freq = as.data.frame(table(freq_append$word))
+colnames(freq)[1] = "word"
+tf_IDF = right_join(freq_append, freq, by = "word")
 num_words = dim(all_words)[1]
 
-testerino$weight = (1/testerino$total.x)*log2(num_words/testerino$Freq)
-testerino$afinn_tfidf = testerino$weight * testerino$score
-testerino$bing_tfidf = testerino$weight * as.numeric(testerino$sentiment)
+# Find words per tweet
+word_count = freq_append %>% 
+  group_by(index.x) %>% 
+  summarize(word = paste(word, collapse = " "))
+word_count$total = str_count(word_count$word, '\\s+')+1
+word_count = right_join(word_count, tf_IDF, by = "index.x")
 
-weighted_model = cbind.data.frame(testerino$index.x, testerino$afinn_tfidf, testerino$bing_tfidf, as.numeric(testerino$emotion.y))
+# Calculate the TF-IDF weight
+tf_IDF$weight = (1/word_count$total)*log(num_words/tf_IDF$Freq)
+tf_IDF$afinn_tfidf = tf_IDF$weight * tf_IDF$score
+tf_IDF$bing_tfidf = tf_IDF$weight * as.numeric(tf_IDF$sentiment)
+
+# Combine the weights per tweet
+weighted_model = cbind.data.frame(tf_IDF$index.x, tf_IDF$afinn_tfidf, tf_IDF$bing_tfidf, as.numeric(tf_IDF$emotion.y))
 colnames(weighted_model) = c("index", "afinn", "bing", "emotion")
-
 weighted_model = weighted_model %>%
   group_by(index = index %/% 1) %>%
   summarise(afinn = sum(afinn), bing = sum(bing))
@@ -229,3 +245,4 @@ plot(best,dat[train,])
 # Predict with tuned radial
 pred = predict(best,dat[-train,])
 table(predict = pred, truth = dat[-train,]$y)
+
